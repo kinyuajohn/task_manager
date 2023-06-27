da1e1705df99ef6b45ad952756821be11b3c0fa1
John Kinya <kingjohnkn@gmail.com>
2023-06-27 16:33:45 +0300

    add task model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=85)
+    content = models.CharField(max_length=300)
+    date_posted = models.DateTimeField(auto_now_add=True)
